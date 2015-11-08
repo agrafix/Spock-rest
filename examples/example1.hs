0912786e6cb82512c776f32a90027e55eba1e281
Alexander Thiemann <mail@athiemann.net>
2015-11-09 00:16:06 +0100

    allow different request and response types

--- examples/example1.hs
+++ examples/example1.hs
@@ -1,15 +1,27 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
 module Main where
 
-import Data.Aeson
-import Data.HVect
-import Web.Spock.Rest
+import           Data.Aeson
+import           Data.HVect
+import           Web.Spock.Rest
 
 newtype Req = Req Bool
     deriving (FromJSON)
 newtype Resp = Resp Bool
     deriving (ToJSON)
+
+data SampleContentType = SampleContentType
+
+instance ContentType SampleContentType where
+    ctMimeType _ = "foo/bar"
+
+instance ContentReader Req SampleContentType where
+    crDecode _ bs =
+        if bs == "42"
+        then Right (Req True)
+        else Left "Request has to be 42"
 
 handleReq :: Req -> Int -> ActionCtxT ctx IO Resp
 handleReq (Req i) val =
@@ -18,4 +30,5 @@ handleReq (Req i) val =
 main :: IO ()
 main =
     runSpock 3000 $ spockT id $
-    get (singleton JSON) ("foo" <//> var) handleReq
+    do post (SampleContentType :~> JSON :|: JSON :~> JSON :|: CtNull) ("foo" <//> var) handleReq
+       get (Only JSON :|: CtNull) "load" $ \() -> return $ Resp True
--- src/Web/Spock/Rest.hs
+++ src/Web/Spock/Rest.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE GADTs                 #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
@@ -16,6 +17,7 @@ module Web.Spock.Rest
       -- * Rendering routes
     , S.renderRoute
       -- * Content Encoders
+    , CTypes(..), (:~>)(..), NoReqBody
     , ContentType(..), ContentWriter(..), ContentReader(..)
     , JSON(..)
       -- * Hooking routes
@@ -28,6 +30,8 @@ module Web.Spock.Rest
     , S.SafeAction (..)
     , S.safeActionPath
     , module Web.Spock.Shared
+      -- * Helper classes and families
+    , AllAre, AllHave
     )
 where
 
@@ -48,31 +52,31 @@ import qualified Web.Spock                 as S
 import           Web.Spock.Shared
 
 -- | Specify an action that will be run when the HTTP verb 'GET' and the given route match
-get :: RestCallable cts req resp xs n f ctx m => HVect cts -> Path xs -> f -> S.SpockCtxT ctx m ()
+get :: (AllAre NoReqBody reqs, RestCallable reqs resps req resp xs n f ctx m) => CTypes reqs resps -> Path xs -> f -> S.SpockCtxT ctx m ()
 get = wire S.GET
 
 -- | Specify an action that will be run when the HTTP verb 'POST' and the given route match
-post :: RestCallable cts req resp xs n f ctx m => HVect cts -> Path xs -> f -> S.SpockCtxT ctx m ()
+post :: RestCallable reqs resps req resp xs n f ctx m => CTypes reqs resps -> Path xs -> f -> S.SpockCtxT ctx m ()
 post = wire S.POST
 
 -- | Specify an action that will be run when the HTTP verb 'GET'/'POST' and the given route match
-getpost :: RestCallable cts req resp xs n f ctx m => HVect cts -> Path xs -> f -> S.SpockCtxT ctx m ()
+getpost :: RestCallable reqs resps req resp xs n f ctx m => CTypes reqs resps -> Path xs -> f -> S.SpockCtxT ctx m ()
 getpost r a = wire S.POST r a >> wire S.GET r a
 
 -- | Specify an action that will be run when the HTTP verb 'HEAD' and the given route match
-head :: RestCallable cts req resp xs n f ctx m => HVect cts -> Path xs -> f -> S.SpockCtxT ctx m ()
+head :: RestCallable reqs resps req resp xs n f ctx m => CTypes reqs resps -> Path xs -> f -> S.SpockCtxT ctx m ()
 head = wire S.HEAD
 
 -- | Specify an action that will be run when the HTTP verb 'PUT' and the given route match
-put :: RestCallable cts req resp xs n f ctx m => HVect cts -> Path xs -> f -> S.SpockCtxT ctx m ()
+put :: RestCallable reqs resps req resp xs n f ctx m => CTypes reqs resps -> Path xs -> f -> S.SpockCtxT ctx m ()
 put = wire S.PUT
 
 -- | Specify an action that will be run when the HTTP verb 'DELETE' and the given route match
-delete :: RestCallable cts req resp xs n f ctx m => HVect cts -> Path xs -> f -> S.SpockCtxT ctx m ()
+delete :: RestCallable reqs resps req resp xs n f ctx m => CTypes reqs resps -> Path xs -> f -> S.SpockCtxT ctx m ()
 delete = wire S.DELETE
 
 -- | Specify an action that will be run when the HTTP verb 'PATCH' and the given route match
-patch :: RestCallable cts req resp xs n f ctx m => HVect cts -> Path xs -> f -> S.SpockCtxT ctx m ()
+patch :: RestCallable reqs resps req resp xs n f ctx m => CTypes reqs resps -> Path xs -> f -> S.SpockCtxT ctx m ()
 patch = wire S.PATCH
 
 -- | Convert 'HVect' to 'H.HList'
@@ -86,19 +90,41 @@ type family HVectLenH (ts :: [*]) :: HNat where
     HVectLenH (t ': ts) = 'HSucc (HVectLenH ts)
 
 -- | Type constraints for a rest callable function
-type RestCallable cts req resp xs n f ctx m =
-    ( AllHave ContentType cts
-    , AllHave (ContentReader req) cts, AllHave (ContentWriter resp) cts
+type RestCallable reqs resps req resp xs n f ctx m =
+    ( AllHave ContentType reqs, AllHave ContentType resps
+    , AllHave (ContentReader req) reqs, AllHave (ContentWriter resp) resps
     , HasRep xs, n ~ HVectLenH (req ': xs)
     , HCurry' n f (req ': xs) (S.ActionCtxT ctx m resp)
     , ArityFwd f n, ArityRev f n
     , MonadIO m
     )
 
+-- | Proof that all elements in a type level list are equal to the first param
+type family AllAre (x :: *) (xs :: [*]) :: Constraint where
+    AllAre x '[] = 'True ~ 'True
+    AllAre x (y ': ys) = (x ~ y, AllAre x ys)
+
 -- | Proof that all types in a list conform to a constraint
 type family AllHave (c :: * -> Constraint) (xs :: [*]) :: Constraint where
     AllHave x '[] = 'True ~ 'True
     AllHave x (y ': ys) = (x y, AllHave x ys)
+
+-- | List that maps request content types to response content types
+data CTypes (reqs :: [*]) (resps :: [*]) where
+  CtNull :: CTypes '[] '[]
+  (:|:) :: (req :~> resp) -> CTypes as bs -> CTypes (req ': as) (resp ': bs)
+
+infixr 5 :|:
+
+-- | Type level information indicating that clients will not send any data
+data NoReqBody
+
+-- | Map a request content type to a response content type
+data (:~>) req resp where
+    -- | Content-Type header must match the requests mimeType and Accept header the responses mimeType
+    (:~>) :: req -> resp -> req :~> resp
+    -- | Accept header must match the responses mimeType
+    Only :: resp -> NoReqBody :~> resp
 
 -- | Data that can be parsed and/or serialized to json
 data JSON = JSON
@@ -110,9 +136,17 @@ class ContentType ct where
 instance ContentType JSON where
     ctMimeType _ = "application/json"
 
+instance ContentType NoReqBody where
+    ctMimeType _ = error "Library error, this should never be called"
+
+-- | Parse a value from a request bytestring
 class ContentReader a ct where
     crDecode :: Proxy ct -> BS.ByteString -> Either String a
 
+instance ContentReader () NoReqBody where
+    crDecode _ _ = Right ()
+
+-- | Serialize a value to a request bytestring
 class ContentWriter a ct where
     cwEncode :: Proxy ct -> a -> BS.ByteString
 
@@ -133,10 +167,10 @@ matchesMimeType t needle =
 
 -- | Specify an action that will be run when a HTTP verb and the given route match
 wire ::
-    forall cts req resp xs n f ctx m.
-    ( RestCallable cts req resp xs n f ctx m )
+    forall reqs resps req resp xs n f ctx m.
+    ( RestCallable reqs resps req resp xs n f ctx m )
     => S.StdMethod
-    -> HVect cts
+    -> CTypes reqs resps
     -> S.Path xs
     -> f
     -> S.SpockCtxT ctx m ()
@@ -145,30 +179,45 @@ wire m ctypes path a =
         fun = hUncurry a
 
         matcherLoop ::
-            forall yts.
-            ( AllHave ContentType yts
-            , AllHave (ContentReader req) yts
-            , AllHave (ContentWriter resp) yts
-            ) => T.Text -> HVect xs -> HVect yts -> ActionCtxT ctx m ()
+            forall as bs.
+            ( AllHave ContentType as, AllHave ContentType bs
+            , AllHave (ContentReader req) as
+            , AllHave (ContentWriter resp) bs
+            ) => T.Text -> HVect xs -> CTypes as bs -> ActionCtxT ctx m ()
         matcherLoop accept captures cts =
             case cts of
-                HNil ->
+                CtNull ->
                     do S.setStatus status500
                        text "Invalid request: Can not handle required Content-type."
-                ((_ :: x) :&: xs) ->
-                    let p :: Proxy x
-                        p = Proxy
-                    in if accept `matchesMimeType` ctMimeType p
-                       then do bsBody <- S.body
-                               case crDecode p bsBody of
-                                   Left errMsg ->
-                                    do S.setStatus status500
-                                       text $ T.pack $ "Invalid JSON: " ++ errMsg
-                                   Right (req :: req) ->
-                                    do (result :: resp) <- fun (H.HCons req (vectToHlist captures))
-                                       S.setHeader "Content-Type" (ctMimeType p)
-                                       S.bytes (cwEncode p result)
-                       else matcherLoop accept captures xs
+                ((rule :: x :~> y) :|: xs) ->
+                    let px :: Proxy x
+                        px = Proxy
+                        py :: Proxy y
+                        py = Proxy
+                        bodyHandler runCond =
+                            do res <- runCond
+                               if res
+                               then do bsBody <- S.body
+                                       case crDecode px bsBody of
+                                           Left errMsg ->
+                                            do S.setStatus status500
+                                               text $ T.pack $ "Invalid JSON: " ++ errMsg
+                                           Right (req :: req) ->
+                                            do (result :: resp) <- fun (H.HCons req (vectToHlist captures))
+                                               S.setHeader "Content-Type" (ctMimeType py)
+                                               S.bytes (cwEncode py result)
+                               else matcherLoop accept captures xs
+                    in case rule of
+                        Only _ ->
+                            bodyHandler $ return $ accept `matchesMimeType` ctMimeType py
+                        (_ :~> _) ->
+                            bodyHandler $
+                            do mContentType <- header "content-type"
+                               case mContentType of
+                                   Nothing ->
+                                        return False
+                                   Just t ->
+                                        return $ accept `matchesMimeType` ctMimeType py && t `matchesMimeType` ctMimeType px
 
         handler :: HVect xs -> S.ActionCtxT ctx m ()
         handler captures =
